046eb4d3746f455fd4b249f3ce4ab2f70221fdc3
Xem0n <biedukiewicz87@gmail.com>
2021-09-29 19:20:48 +0200

    moved blueprints to seperated module

---
+++ backend/flaskr/views/auth.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, request
+from flask_jwt_extended import (
+    jwt_required, 
+    get_jwt, 
+    get_jwt_identity,
+    get_current_user,
+    create_access_token
+)
+
+from flaskr.auth import authenticate, token_blacklist
+from flaskr.db.user import User
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=['POST'])
+def register():
+    name = request.form.get('name', '')
+    password = request.form.get('password', '')
+
+    new_user = User(name=name, password=password)
+
+    try:
+        new_user.is_valid()
+    except Exception as e:
+        return {'error': str(e)}, 406
+    else:
+        new_user.encrypt()
+        new_user.save()
+
+        return {'msg': 'Ok'}, 200
+
+@bp.route('/login', methods=['POST'])
+@jwt_required(optional=True)
+def login():
+    user = get_current_user()
+
+    if user:
+        return {'error': 'Already logged in!'}, 403
+    else:
+        name = request.form.get('name', '')
+        password = request.form.get('password', '')
+        user = authenticate(name, password)
+
+        if user:
+            token = create_access_token(identity=user.id)
+
+            return {'token': token}
+        else:
+            return {'error': 'Wrong credentials!'}, 406
+
+@bp.route('/logout', methods=['DELETE'])
+@jwt_required()
+def logout():
+    jti = get_jwt()['jti']
+    token_blacklist.add(jti)
+
+    return {'msg': 'Access token revoked'}
+
+# test purposes
+@bp.route('/protected')
+@jwt_required()
+def protected():
+    user = get_jwt_identity()
+
+    return {'user': user}
