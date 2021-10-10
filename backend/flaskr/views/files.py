fc0fb35f7e8c06e123e5764dd4b1b3601e3d5374
Xem0n <biedukiewicz87@gmail.com>
2021-10-10 22:36:36 +0200

    feat: download files

--- backend/flaskr/views/files.py
+++ backend/flaskr/views/files.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
-from flask.globals import request
+from flask.globals import current_app, request
+from flask.helpers import send_from_directory
 from flask_jwt_extended import jwt_required, get_current_user
+from flaskr.db import user
 from flaskr.db.file import File
 
 bp = Blueprint('files', __name__, url_prefix='/files')
@@ -8,7 +10,18 @@ bp = Blueprint('files', __name__, url_prefix='/files')
 @bp.route('/<int:file_id>', methods=['GET'])
 @jwt_required()
 def download(file_id):
-    return {'msg': 'ok'}
+    user = get_current_user()
+    file = File.query.filter_by(id=file_id, user_id=user.id).first()
+
+    if file:
+        return send_from_directory(
+            '../' + current_app.config['UPLOAD_FOLDER'],
+            file.get_filename(),
+            as_attachment=True,
+            attachment_filename=file.name
+        )
+    else:
+        return {'error': 'Invalid id!'}, 406
 
 @bp.route('/', methods=['POST'])
 @jwt_required()
