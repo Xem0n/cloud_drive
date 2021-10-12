bc4a5101d7b43af327408738d6b77e2d54b4458a
Xem0n <biedukiewicz87@gmail.com>
2021-10-12 19:42:34 +0200

    refactor: exports file requirement to decorator

--- backend/flaskr/views/files.py
+++ backend/flaskr/views/files.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from flask import Blueprint
 from flask.globals import current_app, request
 from flask.helpers import send_from_directory
@@ -7,21 +8,33 @@ from flaskr.db.file import File
 
 bp = Blueprint('files', __name__, url_prefix='/files')
 
+def file_required(func):
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        user = get_current_user()
+        file_id = kwargs.get('file_id', 0)
+        file = File.query.filter_by(id=file_id, user_id=user.id).first()
+
+        if not file:
+            return {'error': 'Invalid id!'}, 406
+
+        del kwargs['file_id']
+        kwargs['file'] = file
+
+        return func(*args, **kwargs)
+    
+    return decorator
+
 @bp.route('/<int:file_id>', methods=['GET'])
 @jwt_required()
-def download(file_id):
-    user = get_current_user()
-    file = File.query.filter_by(id=file_id, user_id=user.id).first()
-
-    if file:
-        return send_from_directory(
-            '../' + current_app.config['UPLOAD_FOLDER'],
-            file.get_filename(),
-            as_attachment=True,
-            attachment_filename=file.name
-        )
-    else:
-        return {'error': 'Invalid id!'}, 406
+@file_required
+def download(file):
+    return send_from_directory(
+        '../' + current_app.config['UPLOAD_FOLDER'],
+        file.get_filename(),
+        as_attachment=True,
+        attachment_filename=file.name
+    )
 
 @bp.route('/', methods=['POST'])
 @jwt_required()
