936a80d6bea124147e1f8ad3a59da1c86f5e6929
Xem0n <biedukiewicz87@gmail.com>
2021-11-13 21:39:35 +0100

    change routes to new form component

--- frontend/src/components/Router.tsx
+++ frontend/src/components/Router.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import Form, { FormTypes } from './Form';
 
 const Router = () => (
     <Routes>
         <Route path='/' element={<h1>main page</h1>} />
-        <Route path='login' element={<h1>login</h1>} />
+        <Route path='login' element={<Form type={FormTypes.Login} />} />
+        <Route path='register' element={<Form type={FormTypes.Register} />} />
     </Routes>
 );
 
