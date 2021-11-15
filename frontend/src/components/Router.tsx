7964fc3cc1890681f317b62b094f65e1d989f9e8
Xem0n <biedukiewicz87@gmail.com>
2021-11-15 21:57:36 +0100

    add transition

--- frontend/src/components/Router.tsx
+++ frontend/src/components/Router.tsx
@@ -1,12 +1,30 @@
+import { useLocation } from 'react-router';
 import { Routes, Route } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Form, { FormTypes } from './Form';
+import './Router.css';
 
-const Router = () => (
-    <Routes>
-        <Route path='/' element={<h1>main page</h1>} />
-        <Route path='login' element={<Form type={FormTypes.Login} />} />
-        <Route path='register' element={<Form type={FormTypes.Register} />} />
-    </Routes>
-);
+// todo
+// add conditional transition
+// and fix overriding (?) forms
+
+const Router = () => {
+    const location = useLocation();
+
+    return (
+        <TransitionGroup component={null}>
+            <CSSTransition
+                key={location.key}
+                timeout={400}
+                classNames={'route'}>
+                <Routes>
+                    <Route path='/' element={<h1>main page</h1>} />
+                    <Route path='login' element={<Form type={FormTypes.Login} />} />
+                    <Route path='register' element={<Form type={FormTypes.Register} />} />
+                </Routes>
+            </CSSTransition>
+        </TransitionGroup>
+    );
+};
 
 export default Router;
